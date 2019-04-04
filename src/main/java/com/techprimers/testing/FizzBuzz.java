ba7b90335bb2b39e072c1f3d9a63bd50fcb03330
ClementMoral <clement@kivu.tech>
2019-04-04 16:50:06 +0200

    changed fizzz to fizz

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
     public String play(int number) {
 
         if (number == 0) throw new IllegalArgumentException("Number must not be 0");
-        if (number % 3 == 0) return "Fizz";
+        if (number % 3 == 0) return "Fizzz";
         if (number % 5 == 0) return "Buzz";
 
 
