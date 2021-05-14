8d795576c1e45221226b4773337b71a53a304807
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-05-14 19:04:02 +0530

    Update FizzBuzz.java

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 
     public String play(int number) {
 
-        if (number == 0) throw new IllegalArgumentException("Number must not bee 0");
+        if (number == 0) throw new IllegalArgumentException("Number  hello must not bee 0");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
