9e4e92526fa1e851f97c6bc5cc4d5bae27004387
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-08-10 09:34:31 +0530

    new change

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 
     public String play(int number) {
 
-        if (number == 0) throw new IllegalArgumentException("modify Number...another modf for github");
+        if (number == 0) throw new IllegalArgumentException("modify Number...new change");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
