c20f48b17ee2d06c57d63edfe453cc0c601217d9
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-10-14 07:16:39 +0530

    change for pipeline

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 
     public String play(int number) {
 
-        if (number == 0) throw new IllegalArgumentException("modify Number...another chnage");
+        if (number == 0) throw new IllegalArgumentException("change for pipeline");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
