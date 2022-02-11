5b552f682a821460809290745d6b3c60f6c50260
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2022-02-11 09:25:26 +0530

    modify repo

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 
     public String play(int number) {
 
-        if (number == 0) throw new IllegalArgumentException("test new commit for webhook");
+        if (number == 0) throw new IllegalArgumentException("modify on repo");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
