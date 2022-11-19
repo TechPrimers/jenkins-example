e9eae44eb857fee178aa108a0f6fe2e96c593c51
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2022-11-19 06:53:07 +0530

    github webhook change

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 
     public String play(int number) {
 
-        if (number == 0) throw new IllegalArgumentException("new change for webhook");
+        if (number == 0) throw new IllegalArgumentException("github webhook change");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
