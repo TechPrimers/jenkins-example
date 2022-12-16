bafd2bb102aa39cafb0402d205504df9c6f003b6
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2022-12-16 12:17:48 +0530

    test for github hook

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 
     public String play(int number) {
 
-        if (number == 0) throw new IllegalArgumentException("github webhook change");
+        if (number == 0) throw new IllegalArgumentException("test for github hook");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
