29b08529b815559d7200ac9055c9459058265f34
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2023-11-21 12:07:39 +0530

    new commit

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 
     public String play(int number) {
 
-        if (number == 0) throw new IllegalArgumentException("test webhook..");
+        if (number == 0) throw new IllegalArgumentException("new commit on project");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
